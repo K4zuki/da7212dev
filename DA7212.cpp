77f866fcf499f1ed20114de87f9a32cc055fc82e
k4zuki <k4zuki@localhost>
2016-03-22 16:23:14 +0900

    CRLF to LF

--- DA7212.cpp
+++ DA7212.cpp
@@ -26,7 +26,7 @@
 * @section DESCRIPTION
 *    A Driver set for the I2C half of the DA7212
 *
-*/ 
+*/
 
 #include "DA7212.h"
 
@@ -136,9 +136,9 @@ void DA7212::output_mute(bool mute) {
 }
 
 void DA7212::input_power(bool on_off) {
-    
+
     device_adc_pwr = on_off;
-    
+
     if(ADC_source == DA7212_MIC)
     {
         device_mic_pwr = on_off;
@@ -149,14 +149,14 @@ void DA7212::input_power(bool on_off) {
         device_mic_pwr = false;
         device_lni_pwr = on_off;
     }
-    
+
     form_cmd(power_control);
 }
 
 void DA7212::output_power(bool on_off) {
     device_dac_pwr = on_off;
     device_out_pwr = on_off;
-    
+
     form_cmd(power_control);
 }
 
@@ -234,10 +234,10 @@ void DA7212::form_cmd(reg_address add) {
         case line_in_vol_left:
             temp = int(li_vol_left * 32) - 1;
             mute = li_mute_left;
-            
-            if(temp < 0) 
+
+            if(temp < 0)
             {
-                temp = 0; 
+                temp = 0;
                 mute = true;
             }
             cmd = temp & 0x1F;
@@ -246,15 +246,15 @@ void DA7212::form_cmd(reg_address add) {
         case line_in_vol_right:
             temp = int(li_vol_right * 32) - 1;
             mute = li_mute_right;
-            if(temp < 0) 
+            if(temp < 0)
             {
-                temp = 0; 
+                temp = 0;
                 mute = true;
             }
             cmd = temp & 0x1F;
             cmd |= mute << 7;
             break;
-            
+
         case headphone_vol_left:
             temp = int(hp_vol_left * 80) + 47;
             cmd = DA7212_HP_VOL_DF_MASK;
@@ -265,7 +265,7 @@ void DA7212::form_cmd(reg_address add) {
             cmd = DA7212_HP_VOL_DF_MASK;
             cmd |= temp & 0x7F;
             break;
-        
+
         case path_analog:
             temp = int(sdt_vol * 5);
             char vol_code = 0;
@@ -288,13 +288,13 @@ void DA7212::form_cmd(reg_address add) {
             cmd |= mic_mute << 1;
             cmd |= mic_boost_;
             break;
-            
+
         case path_digital:
             cmd |= out_mute << 3;
             cmd |= ((de_emph_code & 0x3) << 1);
             cmd |= ADC_highpass_enable;
             break;
-        
+
         case power_control:
             cmd |= !device_all_pwr << 7;
             cmd |= !device_clk_pwr << 6;
@@ -305,7 +305,7 @@ void DA7212::form_cmd(reg_address add) {
             cmd |= !device_mic_pwr << 1;
             cmd |= !device_lni_pwr << 0;
             break;
-            
+
         case interface_format:
             cmd |= device_master << 6;
             cmd |= device_lrswap << 5;
@@ -329,7 +329,7 @@ void DA7212::form_cmd(reg_address add) {
             cmd |= (temp & 0x03) << 2;
             cmd |= (device_data_form & 0x03);
             break;
-        
+
         case sample_rate:
             temp = gen_samplerate();
             cmd = device_usb_mode;
@@ -337,15 +337,15 @@ void DA7212::form_cmd(reg_address add) {
             cmd |= device_clk_in_div << 6;
             cmd |= device_clk_out_div << 7;
             break;
-            
+
         case interface_activation:
             cmd = device_interface_active;
             break;
-            
+
         case reset_reg:
             cmd = 0;
             break;
-        
+
         case all:
             for( int i = line_in_vol_left; i <= reset_reg; i++)
             {
@@ -363,20 +363,20 @@ void DA7212::defaulter() {
     li_vol_right = DA7212_DF_li_vol_right;
     sdt_vol = DA7212_DF_sdt_vol;
     bypass_ = df_bypass_;
-    
+
     ADC_source = df_ADC_source;
     ADC_source_old = df_ADC_source;
-    
+
     mic_mute = df_mic_mute;
     li_mute_left = df_li_mute_left;
     li_mute_right = df_li_mute_right;
-    
-    
+
+
     mic_boost_ = df_mic_boost_;
     out_mute = df_out_mute;
     de_emph_code = df_de_emph_code;
     ADC_highpass_enable = df_ADC_highpass_enable;
-    
+
     device_all_pwr = df_device_all_pwr;
     device_clk_pwr = df_device_clk_pwr;
     device_osc_pwr = df_device_osc_pwr;
@@ -385,13 +385,13 @@ void DA7212::defaulter() {
     device_adc_pwr = df_device_dac_pwr;
     device_mic_pwr = df_device_mic_pwr;
     device_lni_pwr = df_device_lni_pwr;
-    
+
     device_master         = df_device_master;
     device_lrswap         = df_device_lrswap;
     device_lrws         = df_device_lrws;
     device_bitlength      = df_device_bitlength;
 
-    
+
     ADC_rate  = df_ADC_rate;
     DAC_rate  = df_DAC_rate;
 
@@ -422,5 +422,3 @@ char DA7212::gen_samplerate() {
     }
     return temp;
 }
-
-
